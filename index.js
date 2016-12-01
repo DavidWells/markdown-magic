f1e7d8b5ab3c2cc56b216b7b1aa1163ef6f42d95
davidwells <david@inboundnow.com>
2016-11-30 16:55:13 -0800

    add callback

--- index.js
+++ index.js
@@ -1,55 +1,73 @@
 const path = require('path')
 const fs = require('fs')
 const merge = require('deepmerge')
-const commands = require('./commands')
+const defaultCommands = require('./commands')
 const updateContents = require('./update-contents')
 
-module.exports = function markdownSteriods(mdPath, config) {
-  let markdown = fs.readFileSync(mdPath, 'utf8', (err, contents) => {
-    if (err) {
-      throw err
-    }
-    return contents
-  })
+/**
+ * ```js
+ * markdownSteriods(filename, config, callback)
+ * ```
+ * @param  {string} filePath - Path to markdown file
+ * @param  {object} [config] - configuration object
+ * @param  {Function} [callback] - callback function with updated contents
+ */
+module.exports = function markdownSteriods(filePath, config, callback) {
+  let content
+  try {
+    content = fs.readFileSync(filePath, 'utf8')
+  } catch (e) {
+    console.log(`FILE NOT FOUND ${filePath}`)
+    throw e
+  }
+  /**
+   * ### Configuration Options
+   */
+  const defaultConfig = {
+    /**
+     * `matchWord` - (optional) Comment pattern to look for and replace inner contents
+     * @type {String}
+     * @default [AUTO-GENERATED-CONTENT]
+     */
+    matchWord: 'AUTO-GENERATED-CONTENT',
+    /**
+     * `commands` - (optional) Custom commands to transform block contents
+     * @type {Object}
+     */
+    commands: defaultCommands,
+    /**
+     * `outputPath` - (optional) Change output path of new content. Default behavior is replacing the original file
+     * @type {string}
+     */
+    outputPath: filePath,
+  }
 
   const userConfig = config || {}
-  const defaultConfig = {
-    // Comment pattern to look for and replace inner contents
-    matchWord: 'AUTO-GENERATED-CONTENT',
-    // transform functions
-    commands: commands,
-    // replace original MD file by default
-    outputPath: mdPath,
-    // contents of full MD file
-    markdownContents: markdown
-  }
+  const mergedConfig = merge(defaultConfig, userConfig)
+  // Set originalPath constant
+  mergedConfig.originalPath = filePath
+  // contents of original MD file
+  mergedConfig.originalContents = content
 
-  const mergedOptions = merge(defaultConfig, userConfig)
-  const CONSTANTS = {
-    // original path of md, needed for relative path lookups
-    originalPath: mdPath,
-  }
-  const finalConfig = merge(mergedOptions, CONSTANTS)
-
-  const word = finalConfig.matchWord
-  // pattern /\<\!--.*AUTO-GENERATED-CONTENT:START((.|\n|)*?:END.*--\>)/g
+  /* default regex pattern
+    /\<\!--.*AUTO-GENERATED-CONTENT:START((.|\n|)*?AUTO-GENERATED-CONTENT:END.*--\>)/g
+  */
+  const word = mergedConfig.matchWord
   const regex = new RegExp('\\<\\!--.*'+word+':START((.|\\n|)*?'+word+':END.*--\\>)', 'g')
-  const match = markdown.match(regex)
+  const match = content.match(regex)
 
-  if (match && match.length) {
+  if (match) {
     match.forEach(function(element) {
-       var newContent = updateContents(element, finalConfig)
-       markdown = markdown.replace(element, newContent)
+       var newContent = updateContents(element, mergedConfig)
+       content = content.replace(element, newContent)
     });
     // then write to file
-    fs.writeFileSync(finalConfig.outputPath, markdown)
-    console.log(`${mdPath} updated`)
+    fs.writeFileSync(mergedConfig.outputPath, content)
+    console.log(`${filePath} updated`)
+    callback && callback(content)
   } else {
-    console.log(`no ${word} comment block found in markdown file.
-path: ${mdPath}
-`)
+    console.log(`no ${word} comment block found in markdown file`)
+    console.log(`path: ${filePath}`)
+    callback && callback(content)
   }
 }
-
-
-
