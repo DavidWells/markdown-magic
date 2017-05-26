5686c661cb4e9d316192ececde9e9602c080b402
davidwells <david@inboundnow.com>
2017-05-26 13:22:58 -0700

    update docs to how real usage examples

--- examples/generate-readme.js
+++ examples/generate-readme.js
@@ -4,16 +4,16 @@ const execSync = require('child_process').execSync
 const markdownMagic = require('../index') // 'markdown-magic'
 
 const config = {
-  matchWord: 'MD-MAGIC-EXAMPLE',
+  matchWord: 'MD-MAGIC-EXAMPLE', // default matchWord is AUTO-GENERATED-CONTENT
   transforms: {
-    /* Match AUTO-GENERATED-CONTENT (customTransform:optionOne=hi&optionOne=DUDE) */
+    /* Match <!-- AUTO-GENERATED-CONTENT:START (customTransform:optionOne=hi&optionOne=DUDE) --> */
     customTransform(content, options) {
       console.log('original content in comment block', content)
       console.log('options defined on transform', options)
       // options = { optionOne: hi, optionOne: DUDE}
       return `This will replace all the contents of inside the comment ${options.optionOne}`
     },
-    /* Match AUTO-GENERATED-CONTENT (RENDERDOCS:path=../file.js) */
+    /* Match <!-- AUTO-GENERATED-CONTENT:START (RENDERDOCS:path=../file.js) --> */
     RENDERDOCS(content, options) {
       const fileContents = fs.readFileSync(options.path, 'utf8')
       const docBlocs = require('dox').parseComments(fileContents, { raw: true, skipSingleStar: true })
@@ -23,7 +23,7 @@ const config = {
       })
       return updatedContent.replace(/^\s+|\s+$/g, '')
     },
-    /* Match AUTO-GENERATED-CONTENT (pluginExample) */
+    /* Match <!-- AUTO-GENERATED-CONTENT:START (pluginExample) --> */
     pluginExample: require('./plugin-example')({ addNewLine: true }),
     /* Plugins from npm */
     // count: require('markdown-magic-wordcount'),
