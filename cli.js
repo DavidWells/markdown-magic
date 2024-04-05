b1f9621803c8a33f24ed5fc6b252361da377903a
DavidWells <hello@davidwells.io>
2024-04-05 15:36:31 -0700

    chore: update lib

--- cli.js
+++ cli.js
@@ -3,5 +3,8 @@ const { runCli } = require('./lib/cli')
 const argv = process.argv.slice(2)
 const cliArgs = mri(argv)
 
+/*
+console.log('Raw args:', argv)
+console.log('Before args:', cliArgs)
+/** */
 runCli(cliArgs)
-
